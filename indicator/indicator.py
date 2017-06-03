07221c4c4236abaea55240b36af20346207689ba
Antoine Chabert <chabert.antoine@gmail.com>
2017-06-03 23:50:17 +0200

    Fix issue with quit button

--- indicator/indicator.py
+++ indicator/indicator.py
@@ -1,5 +1,7 @@
+import sys
 from .ui import Ui
 from .backend import Backend
+from threading import Timer
 
 APPINDICATOR_ID = 'coinbase_indicator'
 
@@ -24,19 +26,29 @@ class Indicator(object):
             self.__quit
         )
 
+        self.refresh_thread = None
+
     def run(self):
+        self.__repeat_update()
         self.interface.display()
-        self.interface.display_notification('Indicator started')
-        # TODO refresh every X seconds
+
+    def __repeat_update(self):
+        self.__refresh(True)
+        # TODO make the update time changeable
+        self.refresh_thread = Timer(30.0, self.__repeat_update)
+        self.refresh_thread.start()
 
     def __update_prices(self):
         for crypto_currency in self.client.get_available_crypto_currencies():
             self.prices[crypto_currency] = self.client.get_spot_price(crypto_currency)
 
-    def __refresh(self):
+    def __refresh(self, silent: bool = False):
         self.__update_prices()
         self.interface.update_prices(self.prices)
-        self.interface.display_notification('Prices updated')
+        if not silent:
+            self.interface.display_notification('Prices updated')
 
     def __quit(self):
+        self.refresh_thread.cancel()
         self.interface.close()
+
